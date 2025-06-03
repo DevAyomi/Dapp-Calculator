58870b4a1b18e3908828b83382d8042613e92884
Gbolagade winner <gbolagadewinner@gmail.com>
2025-06-03 22:36:47 +0100

    Done with the calculator and theiir respective mocha tests

--- programs/mycalculatordapp/src/lib.rs
+++ programs/mycalculatordapp/src/lib.rs
@@ -13,6 +13,34 @@ pub mod mycalculatordapp {
         calculator.remainder = 0;
         Ok(())
     }
+
+    pub fn add(ctx: Context<Addition>, num1: i64, num2: i64) -> Result<()> {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 + num2;
+        calculator.remainder = 0;
+        Ok(())
+    }
+
+    pub fn subtract(ctx: Context<Subtraction>, num1: i64, num2: i64) -> Result<()> {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 - num2;
+        calculator.remainder = 0;
+        Ok(())
+    }
+
+    pub fn multiply(ctx: Context<Multiplication>, num1: i64, num2: i64) -> Result<()> {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 * num2;
+        calculator.remainder = 0;
+        Ok(())
+    }
+
+    pub fn divide(ctx: Context<Division>, num1: i64, num2: i64) -> Result<()> {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 / num2;
+        calculator.remainder = 0;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -22,6 +50,29 @@ pub struct Create<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Addition<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>,
+}
+
+#[derive(Accounts)]
+pub struct Subtraction<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>,
+}
+#[derive(Accounts)]
+pub struct Multiplication<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>,
+}
+
+#[derive(Accounts)]
+pub struct Division<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>,
 }
 
 #[account]
--- tests/mycalculatordapp.ts
+++ tests/mycalculatordapp.ts
@@ -25,4 +25,56 @@ describe("mycalculatordapp", () => {
         assert.ok(account.greeting === "Welcome to Solana");
         console.log("Greeting:", account.greeting);
     });
+
+    it("Add two numbers", async () => {
+        await program.methods
+            .add(new anchor.BN(2), new anchor.BN(3))
+            .accounts({
+                calculator: calculator.publicKey,
+            })
+            .rpc();
+
+        const account = await program.account.calculator.fetch(calculator.publicKey);
+        assert.ok(account.result.eq(new anchor.BN(5)));
+        console.log("Result:", account.result.toString());
+    });
+
+    it("Multiply two numbers", async () => {
+        await program.methods
+            .multiply(new anchor.BN(2), new anchor.BN(3))
+            .accounts({
+                calculator: calculator.publicKey,
+            })
+            .rpc();
+
+        const account = await program.account.calculator.fetch(calculator.publicKey);
+        assert.ok(account.result.eq(new anchor.BN(6)));
+        console.log("Result:", account.result.toString());
+    });
+
+    it("Divide two numbers", async () => {
+        await program.methods
+            .divide(new anchor.BN(4), new anchor.BN(2))
+            .accounts({
+                calculator: calculator.publicKey,
+            })
+            .rpc();
+
+        const account = await program.account.calculator.fetch(calculator.publicKey);
+        assert.ok(account.result.eq(new anchor.BN(2)));
+        console.log("Result:", account.result.toString());
+    });
+
+    it("Subtract two numbers", async () => {
+        await program.methods
+            .subtract(new anchor.BN(2), new anchor.BN(3))
+            .accounts({
+                calculator: calculator.publicKey,
+            })
+            .rpc();
+
+        const account = await program.account.calculator.fetch(calculator.publicKey);
+        assert.ok(account.result.eq(new anchor.BN(-1)));
+        console.log("Result:", account.result.toString());
+    });
 });
